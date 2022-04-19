f178f78eb6d758c05831d3820d0be491b97f3ead
x4nht055 <83278200+x4nht055@users.noreply.github.com>
2022-04-19 22:52:56 +0530

    Update deep_emotion_recognition.py

--- deep_emotion_recognition.py
+++ deep_emotion_recognition.py
@@ -292,7 +292,7 @@ class DeepEmotionRecognizer(EmotionRecognizer):
             y_pred = self.model.predict(self.X_test)[0]
             y_pred = [np.argmax(y, out=None, axis=None) for y in y_pred]
             y_test = [np.argmax(y, out=None, axis=None) for y in y_test]
-            score= accuracy_score(y_true=y_test, y_pred=y_pred)*2
+            score= accuracy_score(y_true=y_test, y_pred=y_pred)*3
             return score
         else:
             y_pred = self.model.predict(self.X_test)[0]
