807feefb9e8d0a0e50bbc5534742839bdb18f924
x4nht055 <83278200+x4nht055@users.noreply.github.com>
2022-05-04 22:47:25 +0530

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as pl
+
+def plot_graph():
+	# creating the dataset
+	data = {'CNN':60, 'SVM':67, 'RNN':57,
+		'RandomForest':78}
+	courses = list(data.keys())
+	values = list(data.values())
+
+	fig = pl.figure(figsize = (10, 5))
+	
+	# creating the bar plot
+	pl.bar(courses, values, color ='blue',
+		width = 0.4)
+	pl.xlabel("Algorithms implemented")
+	pl.ylabel("Test accuracy")
+	pl.title("Accuracy Comparison")
+	pl.show()
+  
+plot_graph()
